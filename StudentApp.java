60061aef7efe47c698f681bc5bcf518f6f3ed4db
kylenghiem <32162543+kylenghiem@users.noreply.github.com>
2017-09-26 00:23:14 +1000

    Add files via upload

---
+++ StudentApp.java
@@ -0,0 +1,74 @@
+package uts.wsd;
+
+import java.io.*;
+import javax.xml.bind.*;
+
+public class StudentApp implements Serializable {
+
+    private String filePath;
+    private Students students;
+
+    public StudentApp(String filePath, Students students) {
+        super();
+        this.filePath = filePath;
+        this.students = students;
+    }
+
+    public StudentApp() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public void updateXML(Students students, String filePath) throws Exception {
+        this.students = students;
+        this.filePath = filePath;
+        JAXBContext jc = JAXBContext.newInstance(Students.class);
+        Marshaller m = jc.createMarshaller();
+        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        FileOutputStream fout = new FileOutputStream(filePath);
+        m.marshal(students, fout);
+        fout.close();
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) throws Exception {
+        JAXBContext jc = JAXBContext.newInstance(Students.class);
+        Unmarshaller u = jc.createUnmarshaller();
+        this.filePath = filePath;
+        FileInputStream fin = new FileInputStream(filePath);
+        students = (Students) u.unmarshal(fin);
+        fin.close();
+    }
+
+    public Students getStudents() {
+        System.out.println("xxxxxxxxxxxxx");
+        return students;
+    }
+
+    public void setStudents(Students students) {
+        this.students = students;
+    }
+
+    public void loadStudents() throws JAXBException, FileNotFoundException, IOException {
+        // Create the unmarshaller
+        JAXBContext jc = JAXBContext.newInstance(Students.class);
+        Unmarshaller u = jc.createUnmarshaller();
+
+        // Now unmarshal the object from the file
+        FileInputStream fin = new FileInputStream(filePath);
+        students = (Students) u.unmarshal(fin); // This loads the "shop" object
+        fin.close();
+    }
+
+    public void saveStudents() throws JAXBException, IOException {
+        JAXBContext jc = JAXBContext.newInstance(Students.class);
+        Marshaller m = jc.createMarshaller();
+        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        FileOutputStream fout = new FileOutputStream(filePath);
+        m.marshal(students, fout);
+        fout.close();
+    }
+
+}
